1c0b84ef8a8c6a7d46797cc671c224589431e120
Hu Kaixiang <maxhu787@gmail.com>
2024-02-04 12:31:28 +0800

    fix age counter not working (considering leap years

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ const observerCert = new IntersectionObserver((entries) => {
 const hiddenCerts = document.querySelectorAll('.cert-hidden');
 hiddenCerts.forEach((elem) => observerCert.observe(elem))
 
-
 var age, daysBetweenDates;
+
 daysBetweenDates = function (d1, d2) {
   var diffDays, oneDay;
   oneDay = 24 * 60 * 60 * 1000;
@@ -36,8 +36,14 @@ daysBetweenDates = function (d1, d2) {
 };
 
 age = function () {
-  document.getElementById('myAge').innerText = (daysBetweenDates('Feb 7, 2009 00:00:00', new Date()) / 365).toFixed(8);
+  var birthDate = new Date('Feb 7, 2009 00:00:00');
+  var currentDate = new Date();
+  var years = daysBetweenDates(birthDate, currentDate) / 365.25; // Consider leap years
+  document.getElementById('myAge').innerText = (years.toFixed(8));
 };
+
 setInterval(age, 1200);
+
+
 
 document.getElementById("year").innerHTML = new Date().getFullYear();

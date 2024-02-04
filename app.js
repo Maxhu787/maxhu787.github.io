ff8c7348fdfe9c2c200d421a6918a58c024ec854
Hu Kaixiang <maxhu787@gmail.com>
2024-02-04 18:42:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -47,3 +47,57 @@ setInterval(age, 1200);
 
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
+
+const carouselText = [
+  { text: "Hu Kaixiang", color: "orange" },
+  { text: "胡凱翔", color: "orange" }
+];
+
+document.addEventListener("DOMContentLoaded", async function () {
+  carousel(carouselText, "#feature-text");
+});
+
+async function typeSentence(sentence, eleRef, delay = 100) {
+  const letters = sentence.split("");
+  let i = 0;
+  while (i < letters.length) {
+    await waitForMs(delay);
+    document.querySelector(eleRef).innerHTML += letters[i];
+    i++;
+  }
+  return;
+}
+
+async function deleteSentence(eleRef) {
+  const sentence = document.querySelector(eleRef).innerHTML;
+  const letters = sentence.split("");
+  let i = 0;
+  while (letters.length > 0) {
+    await waitForMs(100);
+    letters.pop();
+    document.querySelector(eleRef).innerHTML = letters.join("");
+  }
+}
+
+async function carousel(carouselList, eleRef) {
+  var i = 0;
+  while (true) {
+    updateFontColor(eleRef, carouselList[i].color);
+    await typeSentence(carouselList[i].text, eleRef);
+    await waitForMs(1500);
+    await deleteSentence(eleRef);
+    await waitForMs(500);
+    i++;
+    if (i >= carouselList.length) { i = 0; }
+  }
+}
+
+function updateFontColor(eleRef, color) {
+  document.querySelector(eleRef).style.color = color;
+}
+
+function waitForMs(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
